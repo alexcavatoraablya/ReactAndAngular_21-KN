e762a15c4f317f3ce65cace5fd8fbfcea0038c1c
CODER <CODER@rivne.itstep.org>
2026-01-16 11:48:21 +0200

    login

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import './App.css'
 import HomePage from "./pages/HomePage.tsx";
 import LoginPage from "./pages/LoginPage.tsx";
+import {Route, Routes} from "react-router-dom";
 
 function App() {
 
   return (
     <>
-        <HomePage/>
-        <LoginPage/>
+        <Routes>
+            <Route path={"/"}>
+                <Route index element={<HomePage/>}/>
+                <Route path={"login"} element={<LoginPage/>}/>
+            </Route>
+        </Routes>
     </>
   )
 }
