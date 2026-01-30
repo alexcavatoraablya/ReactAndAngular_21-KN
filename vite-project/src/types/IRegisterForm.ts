f5e175dc0807300b1f55e85bc56deb58200fcbb4
CODER <CODER@rivne.itstep.org>
2026-01-30 11:45:51 +0200

    form

---
+++ vite-project/src/types/IRegisterForm.ts
@@ -0,0 +1,9 @@
+export interface IRegisterForm {
+    firstName: string;
+    lastName: string;
+    middleName: string;
+    email: string;
+    phone: string;
+    password: string;
+    confirmPassword: string;
+}
