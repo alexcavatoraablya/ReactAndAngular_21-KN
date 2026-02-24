054737fd7662e07f681cc52dfcb5382e4d80e90f
CODER <CODER@rivne.itstep.org>
2026-02-24 10:27:02 +0200

    modal

--- vite-project/src/pages/HomePage/index.tsx
+++ vite-project/src/pages/HomePage/index.tsx
@@ -1,8 +1,10 @@
 import {useState} from "react";
-import type {ICarItem} from "../types/ICarItem.ts";
+import type {ICarItem} from "../../types/ICarItem.ts";
 import {Select} from "antd";
+import ItemCar from "./ItemCar.tsx";
+import CreateCarItem from "./CreateCarItem.tsx";
 
-const HomePage = () =>
+const Homepage = () =>
 {
     //useState - спеціальний хук який призначений для зберігання інформації
     // cars - масив який зберігає інформацію типу ICarItem
@@ -68,6 +70,15 @@ const HomePage = () =>
         }
     }
 
+    const deleteCarHandler = (id: number) => {
+        //console.log("Delete item", id);
+        //будемо змінювати ваш список таким чином,
+        //щоб лишилися уся елементи крім id
+        const result = confirm("Ви дійсно бажаєте видалити елемент?")
+        if(result === true)  //якщо користувач сказав Так - видаляємо
+            setCars(cars.filter(car => car.id !== id));
+    }
+
     return (
         <>
             <h1 className={"text-4xl font-bold text-center mb-4"}>Головна сторінка</h1>
@@ -76,18 +87,18 @@ const HomePage = () =>
                 <Select defaultValue={"default"} style={{ width: 200 }}
                         onChange={(value: string) => sortByPrice(value)}
                         options={[
-                    {
-                        value: "default",
-                        label: "За замовчуванням"
-                    },
-                    {
-                        value: "asc",
-                        label: "Ціна: від меншої до більшої"
-                    },
-                    {
-                        value: "desc",
-                        label: "Ціна: від більшої до меншої"
-                    },
+                            {
+                                value: "default",
+                                label: "За замовчуванням"
+                            },
+                            {
+                                value: "asc",
+                                label: "Ціна: від меншої до більшої"
+                            },
+                            {
+                                value: "desc",
+                                label: "Ціна: від більшої до меншої"
+                            },
                             {
                                 value: "asc",
                                 label: "Рік: від старішої до новішої"
@@ -96,36 +107,14 @@ const HomePage = () =>
                                 value: "desc",
                                 label: "Рік: від новішої до старішої"
                             }
-                ]} />
+                        ]} />
             </div>
+
+            <CreateCarItem/>
             {/*key - змінна забеспечення індентифікації списків у віртуальному DOM*/}
             {cars.map(car =>
-                <div key={car.id} className="max-w-sm w-full lg:max-w-full lg:flex mb-4">
-                    <div
-                        className="h-48 lg:h-auto lg:w-48 flex-none bg-cover rounded-t lg:rounded-t-none lg:rounded-l text-center overflow-hidden"
-                        style={{backgroundImage: `url('${car.image}')`}} title="car">
-                    </div>
-                    <div
-                        className="border-r border-b border-l border-gray-400 lg:border-l-0 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r p-4 flex flex-col justify-between leading-normal">
-                        <div className="mb-8">
-                            <p className="text-sm text-gray-600 flex items-center">
-                                <svg className="fill-current text-gray-500 w-3 h-3 mr-2" xmlns="http://www.w3.org/2000/svg"
-                                     viewBox="0 0 20 20">
-                                    <path
-                                        d="M4 8V6a6 6 0 1 1 12 0v2h1a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2v-8c0-1.1.9-2 2-2h1zm5 6.73V17h2v-2.27a2 2 0 1 0-2 0zM7 6v2h6V6a3 3 0 0 0-6 0z"/>
-                                </svg>
-                                Members only
-                            </p>
-                            {/**/}
-                            <div className="text-gray-900 font-bold text-xl mb-2">{car.mark} {car.model} {car.year} ({car.color})
-                            </div>
-                            <p className="text-gray-700 text-base">{car.description}</p>
-                        </div>
-                        <div className="flex items-center">
-                            <p className={'font-bold text-lg text-gray-900'}>{car.price}₴</p>
-                        </div>
-                    </div>
-                </div>
+                <ItemCar key={car.id} car = {car}
+                    deleteCar={deleteCarHandler}/>
             )}
 
 
@@ -133,4 +122,4 @@ const HomePage = () =>
     );
 }
 
-export default HomePage;
+export default Homepage;
